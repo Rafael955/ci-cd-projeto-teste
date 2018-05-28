6029f938cd979a09f0c785c88510b2419c58b318
Rafael Ferreira Carvalho Caffonso <rafaelcaffonso@gmail.com>
2018-05-28 14:31:51 -0300

    fixing error

--- controllers/pages.test.js
+++ controllers/pages.test.js
@@ -72,7 +72,7 @@ describe('Pages', () => {
     }
     pages.calc(req, res)
     expect(res.send.calledOnce).to.be.true
-    expect(res.send.calledWith('NaoDivisivel')).to.be.true
+    expect(res.send.calledWith('DivisaoZero')).to.be.true
   })
 
   it('should calc properly - nao divisivel zero', () => {
